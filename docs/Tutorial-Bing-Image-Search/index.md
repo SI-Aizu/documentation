9a9761ab2ac25347921940dc9d83776d295d3708
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2020-06-26 16:23:46 +0900

    Add Tutorial-Bing-Image-Search (#114)
    Close #113

---
+++ docs/Tutorial-Bing-Image-Search/index.md
@@ -0,0 +1,64 @@
+# Tutorial
+
+- [Bing Image Search API | Microsoft Azure](https://azure.microsoft.com/en-us/services/cognitive-services/bing-image-search-api/)
+
+
+
+## Start with docker-compose
+
+```sh
+docker-compose build
+docker-compose run --rm dev bash
+```
+
+
+
+## Create your Subscription Key
+
+1. Go to Azure
+1. Create a Resource group
+1. Create a service of the Cognitive Services
+
+!!! note "Check the latest information"
+    ここらへんの手順は頻繁に変わるので公式ドキュメントを参照して最新の情報を確認すること。
+    ここでは解説しない。
+    Check the latest official Azure documentation.
+
+
+
+## Set Your Subscription Key
+
+Create your `settings.ini`.
+
+```ini
+[azure]
+subscription_key = YOUR_KEY
+endpoint = https://YOUR_ENDPOINT.cognitiveservices.azure.com/bing/v7.0/images/search
+```
+
+!!! warning "Make your subscription key private"
+    subscription_key を間違って Git 管理下に含めて、全世界に大公開しないように気をつけること。
+    誤って公開されているトークンはすぐに悪用されます。
+    ちなみにこのディレクトリでは `settings.ini` は `.gitignore` に書かれています。
+
+
+
+## Run the main.py
+
+### Show Usage
+
+```sh
+python main.py --help
+```
+
+### Donload Size
+
+```sh
+python main.py -n 4
+```
+
+### Search Keyword
+
+```sh
+python main.py -k 'cat'
+```
---
+++ docs/Tutorial-Bing-Image-Search/main.py
@@ -0,0 +1,52 @@
+import configparser
+import os
+import datetime
+import argparse
+
+import requests
+from PIL import Image
+from io import BytesIO
+
+
+parser = argparse.ArgumentParser(
+    description="A script for the Bing Image Search"
+)
+num_downloads_default = 4
+parser.add_argument(
+    "-n", "--num-downloads", default=num_downloads_default,
+    help=f"A number of download images. default: {num_downloads_default}"
+)
+keyword_default = 'soccer game'
+parser.add_argument(
+    "-k", "--keyword", default=keyword_default,
+    help=f"A search keyword. default: {keyword_default}"
+)
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('settings.ini')
+    subscription_key = config['azure']['subscription_key']
+    endpoint = config['azure']['endpoint']
+
+    num_downloads = int(args.num_downloads)
+
+    headers = {'Ocp-Apim-Subscription-Key' : subscription_key}
+    search_term = args.keyword
+    params  = {'q': search_term, 'license': 'public', 'imageType': 'photo'}
+    response = requests.get(endpoint, headers=headers, params=params)
+    response.raise_for_status()
+    search_results = response.json()
+    image_urls = [img['thumbnailUrl'] for img in search_results['value'][:num_downloads]]
+
+    dt_now = datetime.datetime.now()
+    save_dir = 'downloads_' + dt_now.strftime('%Y%m%d_%H%M%S')
+    os.makedirs(save_dir)
+    image_count = 0
+    for i in range(num_downloads):
+        image_data = requests.get(image_urls[image_count])
+        image_data.raise_for_status()
+        image = Image.open(BytesIO(image_data.content))
+        image.save(f'{save_dir}/image_{image_count}.jpg')
+        image_count += 1
